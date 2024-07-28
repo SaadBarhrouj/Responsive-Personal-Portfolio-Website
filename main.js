f897cbc9af8015d0b97e167d3669200287661ddd
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2024-07-28 04:07:30 +0200

    UP

---
+++ main.js
@@ -0,0 +1,40 @@
+/*=============== SHOW MENU ===============*/
+const navMenu=document.getElementById('nav-menu'),
+navToggle=document.getElementById('nav-toggle'),
+navClose=document.getElementById('nav-close')
+
+/*=============== MENU show ===============*/
+/*validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===============MENU HIDDEN ===============*/
+/*validate if constant exists */
+if(navClose){
+    navToggle.addEventListener('click',()=>{
+        navClose.classList.remove('show-menu')
+    })
+}
+
+
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== ADD BLUR TO HEADER ===============*/
+
+
+/*=============== EMAIL JS ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
