e0862b909ca5d6d1a422aaf40375f5648e16d32a
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2024-09-23 22:53:18 +0200

    update

---
+++ assets/js/main.js
@@ -0,0 +1,117 @@
+/*=============== SHOW MENU ===============*/
+const navMenu=document.getElementById('nav-menu'),
+navToggle=document.getElementById('nav-toggle'),
+navClose=document.getElementById('nav-close')
+
+/*=============== MENU show ===============*/
+/*validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===============MENU HIDDEN ===============*/
+/*validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink=document.querySelectorAll('.nav__link')
+
+const linkAction = ()=>{
+    const navMenu=document.getElementById('nav-menu')
+    //when we click in each nav__link , we remove the show menu
+    navMenu.classList.remove('show-menu') 
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader=()=>{
+    const header =document.getElementById('header')
+    // when the scroll is greater than 50 viewport height , add the scroll-header class
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+    :header.classList.remove('blur-header')
+}
+window.addEventListener('scroll',blurHeader)
+
+/*=============== EMAIL JS ===============*/
+const contactForm=document.getElementById('contact-form'),
+contactMessage=document.getElementById('contact-message')
+const sendEmail=(e)=>{
+    e.preventDefault()
+    // serviceID - templateID - #form - publickey
+    emailjs.sendForm('service_fqelw6c', 'template_iawjnxg', '#contact-form', 'LNrpj69S0h9V-6GH_')
+    .then(()=> {
+        //show sent message
+        contactMessage.textContent = 'message sent successfully✅'
+        // Remove message after five seconds
+        setTimeout(()=>{
+            contactMessage.textContent=''
+
+        }, 5000)
+        // clear input fields
+        contactForm.reset()
+    }, ()=>{
+        // show error message
+        contactMessage.textContent = 'message not sent (service error)❌'
+
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp =()=>{
+    const scrollUp=document.getElementById('scroll-up')
+    // when the scroll is higher than 350 viewport height , add the show-scroll class to 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section[id]');
+    const scrollActive = () => {
+        const scrollY = window.pageYOffset;
+        sections.forEach(current => {
+            const sectionHeight = current.offsetHeight,
+                sectionTop = current.offsetTop - 58,
+                sectionId = current.getAttribute('id'),
+                sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                sectionsClass.classList.add('active-link');
+            } else {
+                sectionsClass.classList.remove('active-link');
+            }
+        });
+    };
+
+    window.addEventListener('scroll', scrollActive);
+});
+
+
+/*================ SCROLL REVEAL ANIMATION ================*/
+document.addEventListener('DOMContentLoaded', () => {
+    const sr = ScrollReveal({
+      origin: 'top',
+      distance: '60px',
+      duration: 1000,
+      delay: 400,
+     reset: true // animation repeat
+    });
+  
+    sr.reveal('.home__data, .home__social, .contact__container, .footer__container');
+    sr.reveal('.home__image', { origin: 'bottom' });
+    sr.reveal('.about__data, .skills_data', { origin: 'left' });
+    sr.reveal('.about__image, .skills__content', { origin: 'right' });
+    sr.reveal('.services__card, .projects__card', { interval: 100 });
+  });
+  
